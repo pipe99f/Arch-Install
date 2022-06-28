56d9cb08f490a69e2fbac3789aa7ccc29a0b84e4
pipe99g <andres99ga@gmail.com>
2022-06-28 02:18:50 -0500

    pyisntaller

--- softwareanddotfiles.sh
+++ softwareanddotfiles.sh
@@ -27,10 +27,9 @@ yay -S $(cat ~/Arch-install/aurpackages.txt)
 #flatpaks
 flatpak install flathub com.spotify.Client
 
-# vim plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
+# vim packer
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 #pipewire setup
 #systemctl --user daemon-reload
