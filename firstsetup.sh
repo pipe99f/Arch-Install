df1c08151a7ea2b6d889f3ec0a93ff197d3fedbf
pipe99g <andres99ga@gmail.com>
2023-06-30 02:40:27 -0500

    add lts, delete flatpak

--- firstsetup.sh
+++ firstsetup.sh
@@ -4,40 +4,39 @@ ln -sf /usr/share/zoneinfo/America/Bogota /etc/localtime
 hwclock --systohc
 sed -i 's/#en_IN UTF-8/en_IN UTF-8/' /etc/locale.gen
 locale-gen
-echo "LANG=en_IN.UTF-8" >> /etc/locale.conf
-echo "arch" >> /etc/hostname
-echo "127.0.0.1 localhost" >> /etc/hosts
-echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo "LANG=en_IN.UTF-8" >>/etc/locale.conf
+echo "arch" >>/etc/hostname
+echo "127.0.0.1 localhost" >>/etc/hosts
+echo "::1       localhost" >>/etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >>/etc/hosts
 passwd
 
 #important packages NO ESTÁ SIRVIENDO
-pacman -S grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools linux-headers reflector avahi xdg-user-dirs xdg-utils os-prober cups openssh gvfs pipewire pipewire-alsa pipewire-pulse efibootmgr
-
+pacman -S grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools linux-headers reflector avahi xdg-user-dirs xdg-utils os-prober openssh gvfs pipewire pipewire-alsa pipewire-pulse efibootmgr ntp
 
 #grub config
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
-###update-grub se instala con yay, hay que quitar ese comando de acá 
-#echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
-#update-grub
 
-#systemctl enables 
+sed -i 's/#GRUB_SAVEDEFAULT=true/GRUB_SAVEDEFAULT=true/' /etc/default/grub
+sed -i 's/GRUB_DEFAULT=0/GRUB_DEFAULT=saved/' /etc/default/grub
+sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
+
+#systemctl enables
 systemctl enable NetworkManager
-systemctl enable cups.service
 systemctl enable sshd.service
 systemctl enable reflector.timer
 systemctl enable avahi-daemon
+systemctl enable ntpd.service
 
 #user setup
 useradd -m pipe99g
 passwd pipe99g
 usermod -aG wheel pipe99g
-echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers.d/pipe99g
-
+echo "%wheel ALL=(ALL) ALL" >>/etc/sudoers.d/pipe99g
 
 #only for btrfs filesystem
 sed -i 's/MODULES=()/MODULES=(btrfs)/' /etc/mkinitcpio.conf
 
-
-
+#fastest mirrors
+reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
--- softwareanddotfiles.sh
+++ softwareanddotfiles.sh
@@ -1,48 +1,62 @@
 #laptop packages (bluetooth, acpi, wifi, etc.)
 #falta hacer un checkbox que especifique que estos packages son para laptop
 cd
-sudo pacman -S acpi acpi_call tlp bluez bluez-utils
+sudo pacman -S acpi acpi_call tlp bluez bluez-utils brightnessctl
 
 systemctl enable bluetooth.service
 systemctl enable tlp
 
-#enabling multilib
-/etc/pacman.conf
+#Hook that deletes pacman cache
+# mkdir /etc/pacman.d/hooks && touch /etc/pacman.d/clean_pacman_cache.hook
+# tee -a /etc/pacman.d/hooks/clean_pacman_cache.hook << END
+# [Trigger]
+# Operation = Upgrade
+# Operation = Install
+# Operation = Remove
+# Type = Package
+# Target = *
+# [Action]
+# Description = Cleaning pacman cache...
+# When = PostTransaction
+# Exec = /usr/bin/paccache -r
+# END
 
-sudo echo '[multilib]' >> /etc/pacman.conf
-sudo echo '/etc/pacman.d/mirrorlist' >> /etc/pacman.conf
+#enabling multilib
+sudo echo '[multilib]' >>/etc/pacman.conf
+sudo echo 'Include = /etc/pacman.d/mirrorlist' >>/etc/pacman.conf
 sudo pacman -Sy
 
-#arch repository packages
-sudo pacman -S $(cat ~/Arch-install/pacmanpackages.txt)
+#enable parallel downloads
+sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
 
+#install basic packages
+sudo pacman -S "$(cat ~/Arch-install/pacmanpackages.txt)"
+
+#create basic directories
+xdg-user-dirs-update
 
 #YAY installation. NINGUN PAQUETE AUR QUEDÓ INSTALADO CORRECTAMENTE
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
 
-yay -S $(cat ~/Arch-install/aurpackages.txt)
-
-#flatpaks
-flatpak install flathub com.spotify.Client
-
-# vim packer
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+yay -S "$(cat ~/Arch-install/aurpackages.txt)"
 
 #pipewire setup
 #systemctl --user daemon-reload
 #systemctl --user --now disable pulseaudio.service pulseaudio.socket
-
 
 #oh my zsh
 cd
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 #oh my zsh plugins
 cd ~/.oh-my-zsh/plugins/
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/Aloxaf/fzf-tab "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/fzf-tab
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+
+#tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 #icons in terminal
 cd ~/Downloads/
@@ -51,7 +65,7 @@ cd icons-in-terminal/
 ./install.sh
 
 #ranger plugins
- #ranger icons
+#ranger icons
 cd .config/ranger/plugins/
 git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
 
@@ -59,11 +73,21 @@ cd
 git clone https://github.com/pipe99g/dotfiles
 bash stow.sh
 
+#default applications
+handlr set inode/directory thunar.desktop
 
-#El siguiente comando es redundante porque ohmyzsh pregunta si quiero hacer a zsh el shell predeterminado 
+#El siguiente comando es redundante porque ohmyzsh pregunta si quiero hacer a zsh el shell predeterminado
 #chsh -s $(which zsh)
 
-#enable services 
-sudo systemctl enable ntpd
+#Para agregar LTS al GRUB (no sé si así funciona)
+# sudo grub-mkconfig -o /boot/grub/grub.cfg
+# sudo grub-install --efi-directory=/boot
+
+#enable services
 systemctl --user enable pipewire pipewire-pulse
-systemctl enable sshd
+systemctl enable paccache.timer
+systemctl enable ufw.service
+systemctl enable archlinux-keyring-wkd-sync.timer
+systemctl enable cups.service
+#si se usa ssd
+systemctl enable fstrim.timer
